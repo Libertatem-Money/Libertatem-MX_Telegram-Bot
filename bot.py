95fada54d77591867bde456ea0023efebcb8a67b
Libertatem-Money <ivaneijew@gmail.com>
2024-10-25 21:54:33 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 import os
 from telegram import Bot
 
-TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Liest das Token aus den GitHub-Secrets
-CHAT_ID = "-1002399056553"  # Ersetze durch deine Chat-ID
+# Token des Bots - stelle sicher, dass dieser in GitHub als Secret gespeichert ist
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")  # Token aus den GitHub-Secrets abrufen
+CHAT_ID = '-1002399056553'  # Deine Kanal-ID hier
 
 bot = Bot(token=TOKEN)
 
 def send_message():
-    message = "Dies ist ein geplanter Beitrag!"  # Deine Nachricht hier
+    message = "Hello and welcome at Libertatem-MX, do you want to start earn money online?"  # Beispielnachricht
     bot.send_message(chat_id=CHAT_ID, text=message)
 
 if __name__ == "__main__":
